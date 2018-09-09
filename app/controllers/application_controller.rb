bdfd844ae5e0d09bbbb6e9cff5792387604757d3
keisuke-1222 <s.k.s.k.ra20@gmail.com>
2018-09-09 10:03:23 +0900

    Modify css and devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :profile])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :image, :image_cache, :profile])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :email, :profile])
   end
 end
